2ace061a4c5244c61f064c33325cb9f0249829d7
bitanyadereje <bithanyaa.dereje@gmail.com>
2026-02-05 20:57:46 +0300

    BREAK TEST: Intentional failure for CI exercise

--- spec/exercise_demo_spec.rb
+++ spec/exercise_demo_spec.rb
@@ -1,11 +1,11 @@
-﻿# spec/exercise_demo_spec.rb
+# spec/exercise_demo_spec.rb
 require "spec_helper"
 
 RSpec.describe "Docker Lab Exercise 1.2 - FIXED" do
   it "now PASSES after fixing" do
     # Fixed: 10 * 10 = 100
     result = 10 * 10
-    expect(result).to eq(100)  # Correct!
+    expect(result).to eq(999)  # Correct!
   end
   
   it "still PASSES" do
